26f561736ec5ca5ad813ccb1dca2695407e42767
shhu21 <hu.sara21@gmail.com>
2020-07-31 15:26:29 -0700

    adds tests

---
+++ __tests__/addNote.test.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require('path');
+const { addNote } = require('../lib/notes');
+const notes = require('../db/db.json');
+jest.mock('fs');
+
+// pass
+test("adds a new note to the database (pass)", () => {
+  const newNote = {
+    "title": "Note 1",
+    "text": "new note 1",
+    "id": "0"
+  };
+
+  addNote(newNote, notes);
+
+  expect(notes.find(note => note.id == newNote.id)).toBe(newNote);
+});
+
+// fail
+test("adds a new note to the database (fail)", () => {
+  const newNote = {
+    "title": "Note 2",
+    "id": "2"
+  };
+
+  expect(addNote(newNote, notes)).toEqual({
+      "error": {
+      "code": 400,
+      "message": "Invalid object input."
+    }
+  });
+});
