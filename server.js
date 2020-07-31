9c704019661c209000c4a19e7dda6b93568fa999
shhu21 <hu.sara21@gmail.com>
2020-07-30 23:01:38 -0700

    fixes route link issue

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const notes = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
@@ -19,10 +21,11 @@ function updateNotes(notesArr) {
 function addNote(newNote, notesArr) {
     notesArr.push(newNote);
     updateNotes(notesArr);
+    return notesArr;
 }
 
 // ROUTES
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
@@ -30,24 +33,23 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// TODO: not linking
 app.get('/api/notes', (req, res) => {
     console.log('notes')
+    return res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
-    addNote(req.body, notes);
+    return res.send(addNote(req.body, notes));
 });
 
-// TODO: not tested
 app.delete('/api/notes/:id', (req, res) => {
     let newNotesArr = notes;
     for(let i = 0; i < newNotesArr.length; i++) {
         if(newNotesArr[i].id == req.params.id) {
-            newNotesArr.splice(i, 0);
+            newNotesArr.splice(i, 1);
             updateNotes(newNotesArr);
-            break;
+            return res.send(newNotesArr);
         }
     }
 });
